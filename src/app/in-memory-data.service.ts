5acb6b91d4ec16aeb78b72aaaac26a1d094c9f14
Hosaka, Nobuyuki <hosaka.nobuyuki@gmail.com>
2018-08-11 21:48:23 +0900

    Add margin and padding to main. Add data.

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -4,7 +4,7 @@ export class InMemoryDataService implements InMemoryDbService {
   createDb() {
     const owners = [
       { id: 11, name: 'Suzuki Taro', status: true },
-      { id: 12, name: 'Pablo Diego José Francisco de Paula Juan Nepomuceno Cipriano de la Santísima Trinidad Ruiz Picass', status: true },
+      { id: 12, name: 'Pablo Diego José Francisco de Paula Juan Nepomuceno Cipriano de la Santísima Trinidad Ruiz Picasso', status: true },
       { id: 13, name: 'Sato Hanako', status: false },
       { id: 14, name: 'Mobutu Sese Seko Kuku Ngbendu Wa Za Banga', status: false },
       { id: 15, name: 'Alexa Dimitrova', status: true }
